9b2b51e3aac08bf420c5d97300cbeadee4685841
shpkc <tennisflow.contact@gmail.com>
2024-09-23 14:00:21 +0900

    feat : 학력 업데이트

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/Users/sihyungpark/Documents/GitHub/hurlde92_docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/sihyungpark/Documents/GitHub/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/sihyungpark/Documents/GitHub/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/sihyungpark/Documents/GitHub/hurlde92_docs/src/css/custom.css"),
+  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/src/css/custom.css"),
 ];
