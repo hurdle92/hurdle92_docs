5e7427f196d483f0a9563f44a749535ed5295db3
Park Si Hyung <tennisflow.contact@gmail.com>
2024-10-17 00:12:22 +0900

    Fix untracked files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/sihyungpark/dev/github/hurdle92/hurlde92_docs/src/css/custom.css"),
-];
